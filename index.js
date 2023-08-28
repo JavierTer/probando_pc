78260ffc1d317baf345d094dbc46edb9abd9a9b1
Javi <javier.t.sartore@gmail.com>
2023-08-28 15:46:26 -0300

    probamos la consola

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ console.log("probando el 1er cambio en la nueva pc");
 //dejo commiteado localmente esto
 //veo hasta el min 2:29
 console.log("probando nvo log");
-console.log("prueba 3.0");
+console.log("prueba cambiando texto");
