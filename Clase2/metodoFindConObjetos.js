aaadc776e0ed0017a9a36c54702b5871ce1fcffd
Javi <javier.t.sartore@gmail.com>
2023-09-05 10:59:21 -0300

    agrego archivo maps

---
+++ Clase2/metodoFindConObjetos.js
@@ -0,0 +1,16 @@
+const personas = [
+    {
+        nombre: 'Juan'
+
+    },
+    {
+        nombre: 'Pedro'
+    },
+    {
+        nombre: 'Javi'
+    }
+]
+
+const result= personas.find(persona => {return persona.nombre == "Pedro"});
+
+console.log('La personas es : ' + result.nombre);
