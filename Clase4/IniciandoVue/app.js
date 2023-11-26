bdcba746d0ca7829f5657c84954ee15a9b2a400a
Javi <javier.t.sartore@gmail.com>
2023-11-26 17:28:04 -0300

    manejo de directivas de Vue

--- Clase4/IniciandoVue/app.js
+++ Clase4/IniciandoVue/app.js
@@ -15,10 +15,60 @@ este obj es 1 objeto contenedor de propiedades, y vue las convierte en OBSERVABL
 
 
 */
-new Vue({
+const miVue = new Vue({
     el: "#app",
     data: {
-        miTexto: "Hola mundo",
+        miTexto: "Hola mundo", //Esto es reactivo. Esta escuchandose.
+        contador: 0,
+        nombreUsuario: null,
+        miArray: [1,2,4,6,3],
+        series : [
+            {
+                nombre : 'Sherlok Holmes',
+                estreno : 2010,
+                img: 'https://static.wikia.nocookie.net/doblaje/images/b/bd/Sherlock.png/revision/latest?cb=20220125235553&path-prefix=es'
+            },
+            {
+                nombre : 'The good doctor',
+                estreno : 2018,
+                img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbCvC8M7MUYwLl8uyOXvXtUk2akWZHfEWUZA&usqp=CAUttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQOvkykl7EHiNur_cjmBMaKSY94Q_fT1XLGg&usqp=CAU.wikia.nocookie.net/doblaje/images/b/bd/Sherlock.png/revision/latest?cb=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBp0yT-qzU5xTqxrkc25UlloPXPORB2kn4fQ&usqp=CAU&path-prefix=es'
+        
+            },
+            {
+                nombre : 'La ley de los audaces',
+                estreno : 2020,
+                img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2oTEagD313RUTJYGsirwaAoZ-0tGBLeVeFw&usqp=CAU'
+        
+            },
+            {
+                nombre : 'Otra vida',
+                estreno : 2021,
+                img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeSMhTDqKa2oZRf9_Wc4IygLR9-KGLFEpzoQ&usqp=CAU'
+            
+            },
+            {
+                nombre : 'La niñera',
+                estreno : 1996,
+                img: 'https://www.agendasalta.com.ar/public/images/noticias/6575-la-niniera-cumple-65-fue-abusada-sexualmente-lucho-contra-un-cancer-de-utero-y-su-marido-le-confeso-su-homosexualidad.webp'
+        
+            }
+            
+        ]
+    },
+    methods: {
+        cambiarTexto(){
+            //El this hace referencia a la instancia de Vue, y lo necesitamos para 
+            //propiedades y para otros métodos.
+            if(this.nombreUsuario != null) {
+                this.miTexto = "Hola " + this.nombreUsuario
+                this.contador++;
+                console.log(this.contador);
+    
+            }
+        },
+        sacarUltimo(){
+            this.miArray.pop();
+        }
     }
 })
 
